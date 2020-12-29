952461c38a390d223490d12cb47059b8c91d271e
AlexSV <sav75.odessa@gmail.com>
2020-12-29 23:34:40 +0200

    Other tasks from CoderByte

---
+++ src/main/java/LongestWordFromList.java
@@ -0,0 +1,45 @@
+package main.java;
+
+/**
+ * Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string.
+ * If there are two or more words that are the same length, return the first word from the string with that length.
+ * Ignore punctuation and assume sen will not be empty.
+ * Examples
+ * Input: "fun&!! time"
+ * Output: time
+ * Input: "I love dogs"
+ * Output: love
+ */
+
+import java.util.Scanner;
+
+public class LongestWordFromList {
+    public static String LongestWord(String sen) {
+
+        String[] array = sen.split(" ");
+
+            int index = 0;
+            int maxLength = 0;
+
+            for (int i = 0; i < array.length; i++) {
+                if (!array[i].matches("[A-Za-z0-9]+")) {
+                    array[i] = array[i].replaceAll("[^a-zA-Z0-9]", "");
+                }
+
+                if(maxLength < array[i].length()){
+                    maxLength = array[i].length();
+                    index = i;
+                }
+            }
+
+        return array[index];
+    }
+
+
+    public static void main(String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input phrase:");
+        System.out.print(LongestWord(s.nextLine()));
+    }
+}
