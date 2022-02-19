cc1933e5c70841906bec961eb394a42c17cf91d1
alexandr-sviderskiy <alexandr.sviderskiy@forcepoint.com>
2022-02-19 21:34:42 +0200

    added new classes

---
+++ src/main/java/BinaryConverter.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.util.Scanner;
+
+/**
+ * Have the function BinaryConverter(str) return the decimal form of the binary value.
+ * For example: if 101 is passed return 5, or if 1000 is passed return 8.
+  */
+
+public class BinaryConverter {
+    int BinaryConverter(String str) {
+        return Integer.parseInt(str, 2);
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        BinaryConverter c = new BinaryConverter();
+        System.out.print(c.BinaryConverter(s.nextLine()));
+    }
+}
---
+++ src/main/java/FirstReverse.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.util.Scanner;
+
+/**
+ * Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+ */
+
+class FirstReverse {
+    String FirstReverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        FirstReverse c = new FirstReverse();
+        System.out.print(c.FirstReverse(s.nextLine()));
+    }
+
+}
